9dfedca343f185ac24ad1848411a6418242f34b5
João Teixeira <115499150+joaoest@users.noreply.github.com>
2024-09-01 14:06:38 +0100

    session fix

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -1,38 +1,40 @@
 "use client";
 import Image from "next/image";
-import React from "react";
-import { useRouter } from 'next/navigation'
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function getCookieByName(cname: string) {
   if (typeof window !== "undefined") {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
   }
 }
 
-
 export default function IndexPage() {
-  const router = useRouter()
-  const cookie = getCookieByName("session")
-  
-  if(!cookie) {
-    router.push('/login')
-  }
-  
-  return ( 
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <p>Hello you're logged in.</p>
-      </div>
+  const router = useRouter();
+
+  useEffect(() => {
+    const cookie = getCookieByName("session");
+
+    if (!cookie) {
+      router.push("/login");
+    }
+  }, []);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <p>Hello you're logged in.</p>
+    </div>
   );
 }
